5ebfd65093f646cd690f11c2f5097c48207271bf
leonardo.oliveira <leozartino@gmail.com>
2023-01-24 18:35:03 -0300

    refact: move the logic for acces data to a repo

---
+++ src/Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+  public interface IProductRepository
+  {
+    Task<Product> GetProductByIdAsync(int id);
+    Task<IReadOnlyList<Product>> GetProductsAsync();
+  }
+}
