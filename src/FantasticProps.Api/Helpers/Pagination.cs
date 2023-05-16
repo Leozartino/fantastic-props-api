a40a8ff405bdc4b5f38bf0297bfa4121f375e87f
Leonardo Oliveira <leozartino@gmail.com>
2023-05-16 01:58:02 -0300

    change Data property to nullable

--- src/FantasticProps.Api/Helpers/Pagination.cs
+++ src/FantasticProps.Api/Helpers/Pagination.cs
@@ -7,5 +7,5 @@ public class Pagination<T> where T : class
     public int PageIndex { get; set; }
     public int PageSize { get; set; }
     public int Count { get; set; }
-    public IEnumerable<T> Data { get; set; }
+    public IEnumerable<T>? Data { get; set; }
 }
