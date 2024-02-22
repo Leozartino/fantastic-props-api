a62e6366460cff119fd226016bdbd886e922a455
Leonardo Oliveira <leozartino@gmail.com>
2024-02-22 14:31:28 +0100

    feat: add custom response v1

---
+++ src/Core/Models/ResultResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Models
+{
+    public class ResultResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+    }
+}
--- src/FantasticProps.Api/Controllers/BaseApiController.cs
+++ src/FantasticProps.Api/Controllers/BaseApiController.cs
@@ -1,11 +1,40 @@
+using Core.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace FantasticProps.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class BaseApiController : ControllerBase
+public abstract class BaseApiController : ControllerBase
 {
+    protected ActionResult CustomResponse(object result = null)
+    {
+        if (ValidtOperation())
+        {
+            return Ok(new ResultResponse<object>
+            {
+                Success = true,
+                Data = result
+            });
+        }
+
+        return BadRequest(new ResultResponse<object>
+        {
+            Success = false,
+            Errors = GetErrors()
+        }); 
+    }
+
+    public bool ValidtOperation()
+    {
+        return true;
+    }
+
+    protected List<string> GetErrors()
+    {
+        return new List<string> { "error1", "error2" };
+    }
 }
 
 
